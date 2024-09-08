bf843f4672b363c0d8295604263adf0cd6c62168
znichola <niki.zivkovic@hotmail.com>
2024-09-08 22:38:36 +0200

    mid eval world

--- zig/src/world.zig
+++ zig/src/world.zig
@@ -1,10 +1,13 @@
 const std = @import("std");
 const expect = std.testing.expect;
+const expectError = std.testing.expectError;
 
 pub const World = struct {
     map: []u8,
     height: usize,
     width: usize,
+    live: u8 = '#',
+    dead: u8 = '.',
     allocator: std.mem.Allocator,
 
     const DirectionError = error{ OutOfBounds, EmptyWorld };
@@ -32,10 +35,45 @@ pub const World = struct {
         return (World){ .map = m, .width = width, .height = height, .allocator = allocator };
     }
 
-    pub fn count_neighbours(self: World, char: u8, x: i32, y: i32) i32 {
+    pub fn evolve_map(self: World) World {
+        var res = self.clone() catch |err| {
+            std.debug.print("Error cloning world, {}\n", .{err});
+        };
+
+        for (0..self.height) |y| {
+            for (0..self.width) |x| {
+                res.map[y * x] = self.evolve_cell(x, y);
+            }
+        }
+    }
+
+    pub fn evolve_cell(self: World, x: i32, y: i32) u8 {
+        const nc = count_neighbours(self, x, y);
+        var live_cell = false;
+
+        if (self.get_at(x, y)) |cell| {
+            live_cell = cell == self.live;
+        } else |err| {
+            std.debug.print("Error fetching alive stage <{}>\n", .{err});
+        }
+
+        if (live_cell) {
+            if (nc == 2 or nc == 3) {
+                return self.live;
+            }
+        } else {
+            if (nc == 3) {
+                return self.live;
+            }
+        }
+        return self.dead;
+    }
+
+    pub fn count_neighbours(self: World, x: i32, y: i32) i32 {
         var count: i32 = 0;
 
-        //const DirectionFn = fn (self: World, x: usize, y: usize) !u8(DirectionError);
+        // std.debug.print("\nStarting debugg ({}, {})\n", .{ x, y });
+        // const DirectionFn = fn (self: World, x: usize, y: usize) !u8(DirectionError);
         const directions = [_][2]i32{
             .{ 0, -1 }, // North
             .{ 1, 0 }, // East
@@ -51,8 +89,8 @@ pub const World = struct {
             const nx = x + dir[0];
             const ny = y + dir[1];
             if (self.get_at(nx, ny)) |neighbour| {
-                std.debug.print("char is: <{c}>\n", .{neighbour});
-                if (neighbour == char) {
+                std.debug.print("({}, {}): <{c}>\n", .{ nx, ny, neighbour });
+                if (neighbour == self.live) {
                     count += 1;
                 }
             } else |err| {
@@ -67,71 +105,86 @@ pub const World = struct {
         if (self.map.len == 0) {
             return error.EmptyWorld;
         }
-        if (x < 0 or y < 0 or x > self.width or y > self.height) {
+        if (x < 0 or y < 0 or x >= self.width or y >= self.height) {
             return error.OutOfBounds;
         }
         const uw: i32 = @intCast(self.width);
         const pos: usize = @intCast(y * uw + x);
         return self.map[pos];
     }
-
-    //    pub fn get_n(self: World, x: usize, y: usize) !u8(DirectionError) {
-    //        return self.get_at(x, y - 1);
-    //    }
-    //
-    //    pub fn get_e(self: World, x: usize, y: usize) !u8(DirectionError) {
-    //        return self.get_at(x + 1, y);
-    //    }
-    //
-    //    pub fn get_s(self: World, x: usize, y: usize) !u8(DirectionError) {
-    //        return self.get_at(x, y + 1);
-    //    }
-    //
-    //    pub fn get_w(self: World, x: usize, y: usize) !u8(DirectionError) {
-    //        return self.get_at(x - 1, y);
-    //    }
-    //
-    //    pub fn get_ne(self: World, x: usize, y: usize) !u8(DirectionError) {
-    //        return self.get_at(x + 1, y - 1);
-    //    }
-    //
-    //    pub fn get_se(self: World, x: usize, y: usize) !u8(DirectionError) {
-    //        return self.get_at(x + 1, y + 1);
-    //    }
-    //
-    //    pub fn get_sw(self: World, x: usize, y: usize) !u8(DirectionError) {
-    //        return self.get_at(x - 1, y + 1);
-    //    }
-    //
-    //    pub fn get_nw(self: World, x: usize, y: usize) !u8(DirectionError) {
-    //        return self.get_at(x - 1, y - 1);
-    //    }
 };
 
 test "Checking cardinal getters" {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     var test_world = try World.init("123456789", 3, 3, gpa.allocator());
-    //var test_world = World{ .map = "123456789"[0..9], .height = 3, .width = 3 };
 
     try expect(test_world.get_at(1, 1) catch unreachable == '5');
-    //    try expect(test_world.get_n(1, 1) catch unreachable == '2');
-    //    try expect(test_world.get_e(1, 1) catch unreachable == '6');
-    //    try expect(test_world.get_s(1, 1) catch unreachable == '8');
-    //    try expect(test_world.get_w(1, 1) catch unreachable == '4');
-    //
-    //    try expect(test_world.get_ne(1, 1) catch unreachable == '3');
-    //    try expect(test_world.get_se(1, 1) catch unreachable == '9');
-    //    try expect(test_world.get_sw(1, 1) catch unreachable == '7');
-    //    try expect(test_world.get_nw(1, 1) catch unreachable == '1');
+    try expect(test_world.get_at(1, 0) catch unreachable == '2');
+    try expect(test_world.get_at(2, 1) catch unreachable == '6');
+    try expect(test_world.get_at(1, 2) catch unreachable == '8');
+    try expect(test_world.get_at(0, 1) catch unreachable == '4');
+
+    try expect(test_world.get_at(2, 0) catch unreachable == '3');
+    try expect(test_world.get_at(2, 2) catch unreachable == '9');
+    try expect(test_world.get_at(0, 2) catch unreachable == '7');
+    try expect(test_world.get_at(0, 0) catch unreachable == '1');
+
+    try expectError(error.OutOfBounds, test_world.get_at(4, 4));
+    try expectError(error.OutOfBounds, test_world.get_at(-1, 1));
+    try expectError(error.OutOfBounds, test_world.get_at(1, -1));
 }
 
 test "Test World Clone World" {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     var test_world = try World.init("123456789", 3, 3, gpa.allocator());
-    // const test_world = World{ .map = "123456789", .height = 3, .width = 3 };
-    // const foo = gpa.allocator();
     const clone_world = try test_world.clone();
     try expect(std.mem.eql(u8, test_world.map, clone_world.map));
     try expect(test_world.width == clone_world.width);
     try expect(test_world.height == clone_world.height);
+}
+
+// ..##
+// ....
+// ..#.
+// ...#
+
+test "Counting neighbours" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var test_world = try World.init("..##......#....#", 4, 4, gpa.allocator());
+
+    try expect(test_world.count_neighbours(0, 0) == 0);
+    try expect(test_world.count_neighbours(3, 0) == 1);
+    try expect(test_world.count_neighbours(2, 1) == 3);
+    try expect(test_world.count_neighbours(3, 3) == 1);
+}
+
+// .#.
+// ###
+// #..
+
+test "Evolve cells" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var test_world = try World.init(".#.####..", 3, 3, gpa.allocator());
+
+    try expect(test_world.evolve_cell(0, 0) == test_world.live);
+    try expect(test_world.evolve_cell(2, 2) == test_world.dead);
+    try expect(test_world.evolve_cell(1, 2) == test_world.dead);
+    try expect(test_world.evolve_cell(1, 1) == test_world.dead);
+    try expect(test_world.evolve_cell(2, 1) == test_world.live);
+}
+
+// ###
+// #.#
+// ##.
+
+test "Evelove map" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var test_world = try World.init(".#.####..", 3, 3, gpa.allocator());
+
+
+
+    const evolved = test_world.evolve_map();
+
+    test_world.print(out: anytype)
+    
 }
