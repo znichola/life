c450b69682f93f95b37be1fe3e28e58b113bf0f0
znichola <niki.zivkovic@hotmail.com>
2024-11-12 19:55:09 +0100

    getting neighbours

--- haskell/src/Lib.hs
+++ haskell/src/Lib.hs
@@ -2,6 +2,10 @@ module Lib
   ( makeGrid,
     someFunc,
     set,
+    get,
+    getAt,
+    setAt,
+    neighbours,
   )
 where
 
@@ -11,7 +15,25 @@ someFunc = putStrLn "someFunc"
 makeGrid :: Int -> [] Bool
 makeGrid n = replicate (n * n) False
 
-set :: []Bool -> Int -> Bool -> []Bool
+set :: [] Bool -> Int -> Bool -> [] Bool
 set [] _ _ = []
-set (_:xs) 0 val = val : xs
-set (x:xs) n val = x : set xs (n - 1) val
+set (_ : xs) 0 val = val : xs
+set (x : xs) n val = x : set xs (n - 1) val
+
+get :: [] Bool -> Int -> Bool
+get [] _ = False
+get (z : _) 0 = z
+get (_ : zs) n = get zs (n - 1)
+
+getAt :: Int -> [] Bool -> Int -> Int -> Bool
+getAt s bs x y = get bs (x + y * s)
+
+setAt :: Int -> [] Bool -> Int -> Int -> Bool -> [] Bool
+setAt s bs x y = set bs (x + y * s)
+
+neighbours :: Int -> [] Bool -> Int -> Int -> []Bool
+neighbours s bs x y =
+  g (-1) (-1) : g 0 (-1) : g 1 (-1) :
+  g (-1) 0 : g 1 0 :
+  g (-1) 1 : g 0 1 : g 1 1 :
+  [] where g x' y' = getAt s bs (x + x') (y + y')
--- haskell/test/Spec.hs
+++ haskell/test/Spec.hs
@@ -3,9 +3,8 @@
 -- For more exampsles, check out the tests directory.  It contains unit tests
 -- for HUnit.
 
+import Lib (get, getAt, makeGrid, neighbours, set, setAt)
 import Test.HUnit
-
-import Lib (makeGrid, set)
 
 t1 :: Test
 t1 = TestCase (assertEqual "spawning" [False] (makeGrid 1))
@@ -17,7 +16,16 @@ tests =
       "makeGrid 3" ~: [False, False, False, False] ~=? makeGrid 2,
       "set one to true" ~: [True] ~=? set [False] 0 True,
       "set other to true" ~: [False, True, False, False] ~=? set (makeGrid 2) 1 True,
-      "set on empty list?" ~: [] ~=? set [] 0 True
+      "set on empty list?" ~: [] ~=? set [] 0 True,
+      "get one" ~: False ~=? get [False] 0,
+      "get one from many" ~: True ~=? get [False, True, False, False] 1,
+      "get on empty list" ~: False ~=? get [] 0,
+      "get last one" ~: True ~=? get [False, True, False, True] 3,
+      "set last one" ~: [False, False, False, True] ~=? set (makeGrid 2) 3 True,
+      "get at corrd" ~: False ~=? getAt 1 [False] 0 0,
+      "set at corrd" ~: [True] ~=? setAt 1 [False] 0 0 True,
+      "get neighbours" ~: replicate 8 False ~=? neighbours 3 (makeGrid 3) 0 0,
+      "get neighbours 2" ~: True : replicate 7 False ~=? neighbours 3 (set (makeGrid 3) 0 True) 1 1
     ]
 
 main :: IO Counts
